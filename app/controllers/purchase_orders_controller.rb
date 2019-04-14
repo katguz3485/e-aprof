a2c1648d9e2404b753db64f03fe2841a072b2378
katguz3485 <katarzyna.guzik01@gmail.com>
2019-04-14 22:13:48 +0200

    purchase order create working, user orders views added

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,16 +1,35 @@
 # frozen_string_literal: true
 
 class PurchaseOrdersController < ApplicationController
-
-  before_action :set_purchase_order, only: [:show]
-
+  before_action
+  before_action :set_purchase_order, only: %i[show edit update destroy]
+  before_action :set_user_order, only: %i[show edit update destroy]
 
   def index
-    @purchase_orders = PurchaseOrder.where("planned_order_date >= ?", Date.today).page(params[:page]).per(6)
+    @purchase_orders = PurchaseOrder.where('planned_order_date >= ?', Date.today).page(params[:page]).per(6)
   end
 
+  def new
+    @purchase_order = PurchaseOrder.new
+    @user_order = current_user.user_orders.new
 
-  def show
+  end
+
+  def create
+    @purchase_order = PurchaseOrder.new(purchase_order_params)
+    if @purchase_order.save
+      @user_order = current_user.user_orders.new(purchase_order_id: @purchase_order.id)
+      @user_order.save
+      redirect_to user_orders_path, notice: I18n.t('shared.created', resource: 'Purchase Order')
+    else
+      flash.now.alert = I18n.t('shared.error_create')
+      render :new
+    end
+
+
+  end
+
+  def show;
   end
 
   private
@@ -20,7 +39,6 @@ class PurchaseOrdersController < ApplicationController
   end
 
   def purchase_order_params
-    params.require(:purchase_order).permit(:id)
+    params.require(:purchase_order).permit(:id, :name, :grant_id, :planned_order_date)
   end
-
 end
