03e64abf3762acaeb44fe64f456cbab0caee78dc
martom87 <marcintomala87@gmail.com>
2019-04-14 16:00:04 +0200

    items to orders and binding pry added

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,7 +1,22 @@
 class PurchaseOrdersController < ApplicationController
+  before_action :set_purchase_order, only: [:show]
 
   def index
     @purchase_orders = PurchaseOrder.page(params[:page]).per(8)
   end
+
+  def show
+  end
+
+  private
+
+  def set_purchase_order
+    @purchase_order = PurchaseOrder.find(params[:id])
+  end
+
+  def purchase_order_params
+    params.require(:purchase_order).permit(:id)
+  end
+
 
 end
