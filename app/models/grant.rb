ab82502d77e6a915df6844798413000db5201c93
martom87 <marcintomala87@gmail.com>
2019-05-25 19:47:37 +0200

    adding_grant_filter

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -3,4 +3,9 @@
 class Grant < ApplicationRecord
   validates :grant_name, :grant_code, presence: true
   has_many :purchase_orders
+
+  def self.options_for_select
+    Grant.order(grant_name: :desc).map {|grant| [grant.grant_name, grant.id]}
+  end
+
 end
